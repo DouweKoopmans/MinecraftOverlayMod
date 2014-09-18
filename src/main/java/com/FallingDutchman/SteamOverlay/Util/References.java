5df1a17dc818170b45a672bde5e89a77549a32f6
Falling Dutchman <ItzSkillz@users.noreply.github.com>
2014-09-18 08:26:41 +0200

    Updated names in References.java

--- src/main/java/com/FallingDutchman/SteamOverlay/Util/References.java
+++ src/main/java/com/FallingDutchman/SteamOverlay/Util/References.java
@@ -2,9 +2,9 @@ package com.FallingDutchman.SteamOverlay.Util;
 
 public final class References
 {
-    public static final String MOD_ID = "SteamOverlay";
-    public static final String MOD_NAME = "Steam Overlay mod";
-    public static final String VERSION = "0.0.1-alpha";
+    public static final String MOD_ID = "MinecraftOverlay";
+    public static final String MOD_NAME = "Minecraft Overlay mod";
+    public static final String VERSION = "in-dev";
     public static int GUI_COLOR_ONE = 0x991E1E1E;
     public static int GUI_COLOR_TWO = 0x995B5B5B;
     public static final char FORMATTING_CODE_CHAR = '\u00a7';
